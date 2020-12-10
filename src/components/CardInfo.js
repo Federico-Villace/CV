dfb5e16fa6fb44f6d4c18f502c7785b90fdc3da3
Federico Villace <49538811+Federico-Villace@users.noreply.github.com>
2020-12-10 13:55:15 -0300

    bio changes

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -6,13 +6,13 @@ class CardInfo extends React.Component{
     render(){
         return (
             <div className="card mx-auto CV-Info-Card">
-                <div className="card-body">
+                <div >
                     <div className="row center">
-                        <div className="col-6">
-                            <img src={fotocv} className="float-right"/>
+                        <div className="col-2">
+                            <img src={fotocv} className="float-letf"/>
                         </div>
-                        <div className="col-6 CV-Card-Info">
-                            <h2>Bio</h2>
+                        <div className="col-10 CV-Card-Info" >
+                            <h2 align="center">Bio</h2>
                             <p>Mi nombre es Federico Villacé, tengo 25 años, soy Técnico Programador recibido en la Universidad de Lomas de Zamora. Actualmente me encuentro cursando el primer cuatrimestre de la Licenciatura en Gestion de la Información en la misma universidad.<br></br><br></br>
 
                             Estoy en búsqueda de mi primer empleo formal como desarrollador, empleo que pueda desafiarme a poner en práctica mis conocimientos y también entrar en contacto con las problemáticas reales para adquirir otros nuevos.<br></br><br></br>
