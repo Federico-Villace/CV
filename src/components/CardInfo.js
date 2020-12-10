baa35dc0e78939425a633256cd784ee3dbf568bc
Federico Villace <49538811+Federico-Villace@users.noreply.github.com>
2020-12-10 19:40:07 -0300

    estilos en linea

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import fotocv from '../images/fotocv-small.png'
+import fotocv from '../images/fotocv-small.png';
+import backgroundIMG from '../images/background.png';
 import './styles/CardInfo.css'; 
 
 class CardInfo extends React.Component{
     render(){
+        const {title, description, description2, leftColor, rightColor} = this.props; 
         return (
-            <div className="card mx-auto CV-Info-Card">
-                <div >
+            <div className="card mx-auto CV-Info-Card" 
+             style={{
+                backgroundImage : `url(${backgroundIMG}), linear-gradient(to bottom, ${leftColor}, ${rightColor})`
+            }}     
+            >
+                <div>
                     <div className="row center">
                         <div className="col-2">
-                            <img src={fotocv} className="float-letf"/>
-                        </div>
+                            <img src={fotocv} className="float-left"/>
+                        </div> 
                         <div className="col-10 CV-Card-Info" >
-                            <h2 align="center">Bio</h2>
-                            <p>Mi nombre es Federico Villacé, tengo 25 años, soy Técnico Programador recibido en la Universidad de Lomas de Zamora. Actualmente me encuentro cursando el primer cuatrimestre de la Licenciatura en Gestion de la Información en la misma universidad.<br></br><br></br>
-
-                            Estoy en búsqueda de mi primer empleo formal como desarrollador, empleo que pueda desafiarme a poner en práctica mis conocimientos y también entrar en contacto con las problemáticas reales para adquirir otros nuevos.<br></br><br></br>
-
-                            Tengo conocimientos en: <br></br>
-                                Back-End: C#, Java, JS(NodeJS), Python.<br></br>
-                                Front-End: HTML, CSS.<br></br>
-                                Base de datos: SQL Server.</p>
+                            <h2 align="center">{title}</h2> <br></br> 
+                            <p> {description} <br></br>  <br></br> {description2}
+                            </p>
                         </div>
                     </div>
                 </div>  
--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('root');
 
-ReactDOM.render(<CardInfo /> ,container);
+ReactDOM.render(<CardInfo
+                    title="BIO"
+                    description="Mi nombre es Federico Villacé, tengo 25 años, soy Técnico Programador recibido en la Universidad de Lomas de Zamora. Actualmente me encuentro cursando el primer cuatrimestre de la Licenciatura en Gestion de la Información en la misma universidad."   
+                    description2 ="Estoy en búsqueda de mi primer empleo formal como desarrollador, empleo que pueda desafiarme a poner en práctica mis conocimientos y también entrar en contacto con las problemáticas reales para adquirir otros nuevos."
+                    //img="../images/pruebaFondo.png"
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+                /> ,container);
